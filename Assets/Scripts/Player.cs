479dca4508a4c0b9061c49d2b34fedcdf7289b78
borbas09 <salusromelo@gmail.com>
2026-09-22 09:19:07 -0300

    correção de bug de pular infinito
    adicionei metodos OnCollisionEnter2D e OnCollisionExit2D para evitar que o player pule infinitamente

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,13 @@ public class Player : MonoBehaviour
 
     public float speed = 5f;
 
-    [SerializeField] private Rigidbody2D rb;
+    private Rigidbody2D rb;
+
+    private bool isGraunded = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
     void Update()
     {
@@ -18,10 +19,30 @@ public class Player : MonoBehaviour
 
         rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y); //Vai aplicar a velocidade horizontal ao Rigidbody2D
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGraunded)
         {
             rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse); //Vai aplicar uma força vertical ao Rigidbody2D
+
+
         }
 
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGraunded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")){
+            isGraunded = false;
+        }
+
+
+    }
+
 }
